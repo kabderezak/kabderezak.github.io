89b28755a715fb4cd4d369926c5433fd37c55553
Abderezak KAFI <cherir.geni3@gmail.com>
2019-03-28 23:44:20 +0100

    email me done

---
+++ js/myscript.js
@@ -0,0 +1,22 @@
+function myFunction() {
+
+    var fab =document.getElementById('fab');
+    fab.innerHTML = "email copied";
+    fab.style.backgroundColor= '#4D4D4D'
+
+    const el = document.createElement('textarea');  // Create a <textarea> element
+    el.value = "Abderezak.kafi@gmail.com";                               // Set its value to the string that you want copied
+    el.style.position = 'absolute';                 
+    el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+    document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
+    el.select();                                    // Select the <textarea> content
+    document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
+    document.body.removeChild(el);                  // Remove the <textarea> element
+    setTimeout(rename,600);
+  }
+  function rename() {
+
+    var fab =document.getElementById('fab');
+    fab.innerHTML = "Email me";
+    fab.style.backgroundColor= '#166EF8'
+  }
