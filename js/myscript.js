71228637d4c2ef18f235c39377639650fc2c9fba
Abderezak KAFI <cherir.geni3@gmail.com>
2019-04-24 13:40:45 +0100

    js time

--- js/myscript.js
+++ js/myscript.js
@@ -12,7 +12,7 @@ function myFunction() {
     el.select();                                    // Select the <textarea> content
     document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
     document.body.removeChild(el);                  // Remove the <textarea> element
-    setTimeout(rename,1000);
+    setTimeout(rename,1500);
   }
   function rename() {
 
